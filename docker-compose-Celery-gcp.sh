049d2b6276b85b802e044dcfa329135424e11c95
Hoc Leng Chung <hoc.leng.chung@devoteam.com>
2019-09-04 22:46:57 +0200

    remove fuse mount

--- docker-compose-Celery-gcp.sh
+++ docker-compose-Celery-gcp.sh
@@ -1,4 +1,4 @@
 docker build --rm --build-arg AIRFLOW_DEPS="gcp" -t hcl/docker-airflow:1.0 .
 sed 's/image_tag/hcl\/docker-airflow:1.0/g' docker-compose-CeleryExecutor-template.yml > docker-compose-CeleryExecutor-gcp.yml
 docker-compose -f docker-compose-CeleryExecutor-gcp.yml up -d
-gcsfuse -o nonempty airflow_vangogh-231409 ./dags
+#gcsfuse -o nonempty airflow_vangogh-231409 ./dags
