6df165f342eac05102afc7744a1ca322f6252920
Hoc Leng Chung <hoc.leng.chung@devoteam.com>
2019-09-04 21:10:29 +0200

    Created gcp docker yml

---
+++ docker-compose-Celery-gcp.sh
@@ -0,0 +1,3 @@
+docker build --rm --build-arg AIRFLOW_DEPS="gcp" -t hcl/docker-airflow:1.0 .
+sed 's/image_tag/hcl/docker-airflow:1.0/g' docker-compose-CeleryExecutor-template.yml > docker-compose-CeleryExecutor-gcp.yml
+docker-compose -f docker-compose-CeleryExecutor-gcp.yml up -d
