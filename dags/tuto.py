e9029eabb49404ef6bac9b3211139f7f0d815a7b
Hoc Leng Chung <hoc.leng.chung@devoteam.com>
2019-09-04 21:41:10 +0200

    Removed example dags, fixed build script

--- dags/tuto.py
+++ dags/tuto.py
@@ -4,13 +4,14 @@ http://airflow.readthedocs.org/en/latest/tutorial.html
 """
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
+from airflow.contrib.sensors.gcs_sensor import GoogleCloudStorageObjectUpdatedSensor
 from datetime import datetime, timedelta
 
 
 default_args = {
     "owner": "airflow",
     "depends_on_past": False,
-    "start_date": datetime(2015, 6, 1),
+    "start_date": datetime(2019, 9, 3),
     "email": ["airflow@airflow.com"],
     "email_on_failure": False,
     "email_on_retry": False,
@@ -22,27 +23,12 @@ default_args = {
     # 'end_date': datetime(2016, 1, 1),
 }
 
-dag = DAG("tutorial", default_args=default_args, schedule_interval=timedelta(1))
+dag = DAG("open_air", default_args=default_args, schedule_interval='0 0 1 1 *')
 
-# t1, t2 and t3 are examples of tasks created by instantiating operators
-t1 = BashOperator(task_id="print_date", bash_command="date", dag=dag)
-
-t2 = BashOperator(task_id="sleep", bash_command="sleep 5", retries=3, dag=dag)
-
-templated_command = """
-    {% for i in range(5) %}
-        echo "{{ ds }}"
-        echo "{{ macros.ds_add(ds, 7)}}"
-        echo "{{ params.my_param }}"
-    {% endfor %}
-"""
 
 t3 = BashOperator(
     task_id="templated",
-    bash_command=templated_command,
+    bash_command="echo 'Hello World'",
     params={"my_param": "Parameter I passed in"},
     dag=dag,
 )
-
-t2.set_upstream(t1)
-t3.set_upstream(t1)
